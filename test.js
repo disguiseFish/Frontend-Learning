b039d68fe7b21f592ab79ab695e31d8a098b1ddd
ynr <ynr666666@163.com>
2024-08-22 00:46:23 +0800

    test for in of

---
+++ test.js
@@ -0,0 +1,12 @@
+const person = {
+    name: '小熔同学',
+    age: 17,
+    profession: 'Engineer'
+}
+
+for (const key in person) {
+    console.log(key)
+}
+// for (const key of person) {
+//     console.log(key)
+// }
